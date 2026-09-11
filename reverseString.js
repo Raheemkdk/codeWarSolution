2c79e94ab4d310acdeb500e520836c24ff16d52c
Raheem Kadiku <kadikuraheem@gmail.com>
2026-09-11 13:25:49 -0400

    Reverse String solution from CodeWars

---
+++ reverseString.js
@@ -0,0 +1,21 @@
+// Sample Tests
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(solution('world'), 'dlrow');
+    assert.strictEqual(solution('hello'), 'olleh');
+    assert.strictEqual(solution(''), '');
+    assert.strictEqual(solution('h'), 'h');
+  });
+});  
+
+// Solution
+
+function solution(str){
+  let word = Array.from(str)
+  console.log(word.reverse())
+  return(word.join('')) 
+}
