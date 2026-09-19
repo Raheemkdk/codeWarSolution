b5ec61b93bde4f3b1adabf0d8e5d42e15a575cf2
Raheem Kadiku <kadikuraheem@gmail.com>
2026-09-18 22:40:24 -0400

    String repeat solution for codewars

---
+++ stringRepeat.js
@@ -0,0 +1,28 @@
+// Test Case
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Tests", function() {
+  it ("Basic tests", function() {
+    assert.strictEqual(repeatStr(3, "*"), "***");
+    assert.strictEqual(repeatStr(5, "#"), "#####");
+    assert.strictEqual(repeatStr(2, "ha "), "ha ha ");
+    assert.strictEqual(repeatStr(0, ""), "");
+    assert.strictEqual(repeatStr(0, "I"), "");
+    assert.strictEqual(repeatStr(5, ""), "");
+    assert.strictEqual(repeatStr(6, "I"), "IIIIII");
+    assert.strictEqual(repeatStr(5, "Hello"), "HelloHelloHelloHelloHello");
+  });
+});
+// Solution
+function repeatStr (n, s) {
+  let str = ""
+  for(i = 1; i<=n; i++){
+//     console.log(s)
+  str += s
+ console.log(str)
+
+  }
+    return str
+}
